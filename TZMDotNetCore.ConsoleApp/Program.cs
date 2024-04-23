537a6d293359fcd4b2168f8dbd60882e8b4d3b62
Thant-Zin-Moe <142769260+Thant-Zin-Moe@users.noreply.github.com>
2024-04-23 12:33:15 +0630

    homework

--- TZMDotNetCore.ConsoleApp/Program.cs
+++ TZMDotNetCore.ConsoleApp/Program.cs
@@ -32,12 +32,14 @@ Console.WriteLine("Hello, World!");
 //     Console.WriteLine("-------------------------");
 // }
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create("title", "author", "content");
 //adoDotNetExample.Update(1002, "test title", "test author", "test content");
 //adoDotNetExample.Delete(1002);
-adoDotNetExample.Edit(1002);
-adoDotNetExample.Edit(1);
+//adoDotNetExample.Edit(1002);
+//adoDotNetExample.Edit(1);
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 
 Console.ReadKey();
