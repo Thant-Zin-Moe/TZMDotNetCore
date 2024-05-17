06c4c5d08906204c01468f83d640048fe3fc54fd
Thant-Zin-Moe <142769260+Thant-Zin-Moe@users.noreply.github.com>
2024-05-17 16:20:00 +0630

    [Pizza]

---
+++ TZMDotNetCore.PizzaApi/ConnectionStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZMDotNetCore.PizzaApi
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "LENOVO-V14\\SQLEXPRESS",
+            InitialCatalog = "TZMDotNetCore",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        public static object SqlconnectionStringBuilder { get; internal set; }
+
+        //public static object SqlConnectionStringBuilder { get; internal set; }
+    }
+}
