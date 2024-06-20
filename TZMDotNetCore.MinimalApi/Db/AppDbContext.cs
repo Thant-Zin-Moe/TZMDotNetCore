ee98e544d537e7fa6190bbaf150549aa4cba10a3
Thant-Zin-Moe <142769260+Thant-Zin-Moe@users.noreply.github.com>
2024-06-21 00:54:46 +0630

    [Complete] Minimal API CRUD

---
+++ TZMDotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TZMDotNetCore.MinimalApi.Models;
+
+namespace TZMDotNetCore.MinimalApi.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+    //}
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ TZMDotNetCore.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TZMDotNetCore.MinimalApi.Db;
+using TZMDotNetCore.MinimalApi.Models;
+
+namespace TZMDotNetCore.MinimalApi.Features.Blog
+{
+    public static class BlogService
+    {
+        public static IEndpointRouteBuilder MapBlogs(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/blog", async (AppDbContext db) =>
+            {
+
+                var lst = await db.Blogs.AsNoTracking().ToListAsync();
+                return Results.Ok(lst);
+            });
+
+            app.MapPost("/api/blog/", async (AppDbContext db, BlogModel blog) =>
+            {
+                await db.Blogs.AddAsync(blog);
+                var result = await db.SaveChangesAsync();
+                string message = result > 0 ? "Saving successful." : "Saving failed.";
+                return Results.Ok(message);
+            });
+
+            app.MapPut("/api/blog/{id}", async (int id, AppDbContext db, BlogModel blog) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data found!");
+                }
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Updating successful." : "Updating failed.";
+                return Results.Ok(message);
+            });
+
+            app.MapDelete("/api/blog/{id}", async (int id, AppDbContext db) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data found!");
+                }
+
+                db.Blogs.Remove(item);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Deleting successful." : "Deleting failed.";
+                return Results.Ok(message);
+            });
+
+            return app;
+        }
+    }
+}
